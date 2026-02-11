d22321e379642948935395bd69ece09f1b84dde8
Sonechka <148910070+demonsofya@users.noreply.github.com>
2026-02-12 02:46:09 +0300

    Добавлены комментарии

--- USER_STR_PRINTOUT.asm
+++ USER_STR_PRINTOUT.asm
@@ -8,12 +8,12 @@ start:		    mov ax, 0b800h
                 mov bx, 81h         ; номер позиции в строке (позиция в памяти)
                 mov cl, ds:[80h]    ; длина строки
 
-                mov di, 160 * 8
-                add di, 80
-                sub di, cx          ; двигаем в середину
+                mov di, 160 * 8     ; отступ 8 строк
+                add di, 80          ; идем в середину строки
+                sub di, cx          ; двигаем в середину (cx тк каждый символ 2байта)
 
                 xor ax, ax
-                mov ah, 1bh 
+                mov ah, 1bh         ; cyan на фоне темно-синего
                 
             draw:
                 mov al, [bx]
