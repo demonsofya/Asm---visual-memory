8cc3d9807f3edb090c71fdc3f312fae4164dee1e
Sonechka <148910070+demonsofya@users.noreply.github.com>
2026-02-18 16:28:13 +0300

    Добавлены кучу фич через консоль
    Стили рамки, отдельно цвет строки и рамки, деление надписи на строки

--- ALL_RAMKA.asm
+++ ALL_RAMKA.asm
@@ -7,39 +7,98 @@ start:           jmp main
 
 ;--------------------MAIN-------------------------
 ; expects: string, started with space, 
-; 2 hex nums - latter & back color
+; 2 hex nums - ramka latter & back color
 ; 2 hex num - string num
+; 2 hex nums - ramka width 
+; 2 hex nums - user string color
+; 2 hex nums (from 1 to 3) - ramka style
 ; (but is can by any 2 symbols,it just be random color for result)
 ; cx is always string length
 ;--------------------------
-main:           mov ax, 0b800h
-                mov es, ax
+main:           
+        mov ah, 09h
+        mov dx, offset Hello
+        int 21h
 
-                mov bx, 82h         ; номер позиции в строке (позиция в памяти) + перепрыгиваем пробел
-                mov cl, ds:[80h]    ; длина строки - не хватает регистров чтобы это хранить, проще каждый раз обращаться заново
+        xor dx, dx
+        xor ax, ax
 
-                call get_user_num   ; getting color
-                mov dh, al          ; moving color to dh
+        mov ax, 0b800h
+        mov es, ax
 
-                call get_user_num   ; ax = string num
+        mov bx, 82h         ; номер позиции в строке (позиция в памяти) + перепрыгиваем пробел
+        mov cl, ds:[80h]    ; длина строки - не хватает регистров чтобы это хранить, проще каждый раз обращаться заново
 
-                mov bp, ax
-                call get_user_num
-                xchg bp, ax         ; bp = ramka width, ax = string num
-                push bp
+        call get_user_num   ; getting color
+        mov dh, al          ; moving color to dh
 
-                mov si, bx
-                sub si, 81h
-                sub cx, si          ; cx - new length of line ('cause skipping read symbols)
-                call print_user_string
+        call get_user_num   ; ax = string num
 
-                pop bp
-                mov bh, dh
-                call draw_ramka
+        mov bp, ax
+        call get_user_num
+        xchg bp, ax         ; bp = ramka width, ax = string num
 
-                mov ax, 4c00h
-                int 21h
+        mov si, ax
+        call get_user_num
+        xchg si, ax         ; style type
+
+
+        push cx
+        push bx
+        push dx
+
+        cmp si, 1
+        je first_type
+        cmp si, 2
+        je second_type
+        jmp third_type
+
+
+            first_type:
+        push 1ah
+        push 1bh
+        push 18h
+        push 19h
+        jmp call_draw_ramka
+
+            second_type:
+        push 03h
+        push 04h
+        push 05h
+        push 06h
+        jmp call_draw_ramka
+
+            third_type:
+        push 01h
+        push 02h
+        push 0dh
+        push 0eh
+        jmp call_draw_ramka   
+
+            call_draw_ramka:
+        mov cx, 10d
+        mov bh, dh
+        call draw_ramka
+        add sp, 8
+
+        pop dx
+        pop bx
+        pop cx
+
+        xchg dx, ax
+        call get_user_num
+        xchg dx, ax
+        xchg dl, dh
+
+        mov si, bx
+        sub si, 81h
+        sub cx, si          ; cx - new length of line ('cause skipping read symbols)
+        call print_user_string
+
+        mov ax, 4c00h
+        int 21h
 ;--------------------------
+
 
 
 ;--------------------------
@@ -61,10 +120,10 @@ get_user_num proc
         sub al, 48d             ; if it is 0-9
         jmp second_num
 
-    first_hex_letter:           ; if it is a-f
+            first_hex_letter:           ; if it is a-f
         sub al, 87d             ; ascii code of 'a' starts from 87
 
-    second_num:                 ; second num (if its not obvious)
+            second_num:                 ; second num (if its not obvious)
         cmp ah, 57d
         jg  second_hex_letter
 
@@ -74,10 +133,10 @@ get_user_num proc
         sub ah, 48d  
         jmp end
 
-    second_hex_letter:
+            second_hex_letter:
         sub ah, 87d  
 
-    end:
+            end:
         shl al, 4        ; al *= 16 -> to it go in ah like 1st num
         add al, ah       ; like 1st*16 + 2nd -> our num
         xor ah, ah       ; ax = al = hex num, ah = 0
@@ -96,15 +155,19 @@ get_user_num proc
 ;Entry:
 ;   cl - str length | save(!)
 ;   bx - mem point  | destroy
-;   si              | destroy | using us num of mem cell
 ;   ax - ramka string num
 ;   dh - letters color + background color
+;   si              | destroy | using us num of mem cell
 ;   bp              | destroy
+;   di              | destroy
 ;--------------------------
 print_user_string proc
-        mov di, ax          ; сохраняем перед умножением
+        push ax          ; сохраняем перед умножением
         mov bp, dx
         push cx             ; save cx
+
+        mov di, cx
+        mov cx, 10d
 
         mov si, 160d
         mul si              ; отступ строк - dx не меняется потому что байт а не слово
@@ -114,18 +177,42 @@ print_user_string proc
         and si, 0FFFEh      ; making num even, cause colors and symbols will change in opposite
         mov dx, bp
 
-        draw_user_str_cycle:
+        xor bp, bp
+
+            drawing_many_strings:
+        push si             ; saving start position
+        mov cx, 10d
+        add bp, 1
+
+                draw_user_1string:
             mov dl, ds:[bx] 
             mov es:[si], dx
             inc bx
             add si, 2
-            loop draw_user_str_cycle
+            dec di
+            cmp di, 0
+            je draw_end
 
-        xchg ax, di
+            loop draw_user_1string
+
+        pop si
+        add si, 160d
+        jmp drawing_many_strings
+        
+        draw_end:
+        mov es:[si], 03h
+        mov es:[si+1], dh
+        add si, 2
+        loop draw_end
+
+        pop si
+
         pop cx
+        pop ax
 
         ret
 ;--------------------------
+
 
 
 ;--------------------------
@@ -133,22 +220,37 @@ print_user_string proc
 ; void print_symbols_horizontal_string(symbol_color, str_len, string_num)
 ; printing string of str_len symbols in center of string_num string full of symbols symbol_color
 ; return nothing
-;ax - string num    | save(!)
-;di                 | destroy | using num of memory cell
-;si                 | destroy
-;cx - string length | save(!)
-;bx - symbol & color| save(!) not dx because mul
+; Expect:
+;   db = 0b800h
+; Entry:
+;   1st param - symbol & color
+;   2nd param - string length
+;   3rd param - string number
+; Using registers:
+;   ax - string num    | save(!)
+;   cx - string length | save(!)
+;   bx - symbol & color| save(!) not dx because mul
+;   si                 | save(!)
+;   di                 | save(!) using num of memory cell
+; Destroy:
+;   nothing
+; Return:
+;   nothing
 ;--------------------------
 print_symbols_horizontal_string proc 
+        
         push bp
         mov bp, sp
+
+        push si
+        push ax         ; save ax
+        push cx         ; save cx
+        push di
 
         mov ax, [bp + 4]         ; string num
         mov cx, [bp + 6]         ; str len
         mov bx, [bp + 8]         ; symbol, color
 
-        push ax         ; save ax
-        push cx         ; save cx
             
         mov si, 160d
         mul si          ; ax *= 160 - correct string
@@ -160,128 +262,172 @@ print_symbols_horizontal_string proc
         mov ax, bx      ; ax = correct symbol
         rep stosw  
 
+        pop di
         pop cx          
         pop ax
-
+        pop si
         pop bp
+
         ret 6d           ; clearing stack
 ;--------------------------
+
 
 
 ;--------------------------
 ; CDECL CALL TYPE
 ;void print_symbols_vertical_string(string_num, column_num, string_length, symbol&color)
-;ax - string num        (save)
-;dx - column num        (save)
-;di - string length     (save)
-;bx - symbol & color |  (save)
-;si                  |  destroy
+;printing vertical string with string_length symbols started from (column_num, string_num) 
+; Expect:
+;   ex = 0b800h
+; Entry:
+;   1st param - string number
+;   2nd param - column number
+; Using registers:
+;   ax - string num     |  destroy to string_num as arg_1
+;   dx - using for mul  |  destroy
+;   di - string length  |  save
+;   bx - symbol & color |  save
+;   cx - using for loop |  save
+;Destroy:
+;   dx
+;   ax
+;Return:
+;   nothing
 ;--------------------------
 print_symbols_vertical_string proc
         push bp
         mov bp, sp
-
-        mov ax, [bp + 4]         ; string num
-        push ax
-
-        mov si, 160d
-        mul si
-        mov si, ax              ; si = right str
-        pop ax                  ; saving ax
-
-        mov dx, [bp + 6]
-        add si, dx               ; dx = right column
-
-        mov di, [bp + 8]         ; str len
-        mov bx, [bp + 10]        ; symbol, color
         push di
 
-        draw_vert_string_cycle:
-                mov es:[si], bx
-                add si, 160
-                dec di
-                cmp di, 0
-                jne draw_vert_string_cycle 
+        mov ax, ss:[bp + 4]         ; string num
+        push ax
 
-        pop di                    ; saving di
+        mov dx, 160d
+        mul dx
+        mov dx, ax              ; dx = right str
+        pop ax                  ; saving ax (cause mul)
+        
+        add dx, ss:[bp + 6]               ; dx + column
+
+        mov di, ss:[bp + 8]         ; str len
+        mov bx, ss:[bp + 10]        ; symbol, color
+
+        xchg cx, di
+        xchg di, dx
+
+            draw_vert_string_cycle:
+        mov es:[di], bx
+        add di, 160d
+        loop draw_vert_string_cycle 
+
+        xchg di, dx
+        xchg cx, di
+
+        pop di
         pop bp
         ret                       ; dont moving sp 'cause cdecl
 ;--------------------------
 
 
+
 ;--------------------------
 ;draw ramka bp tymes (да у меня кончились регистры)
 ; cx - start str length         | destroy
-; ax - str num
-; bx - color & symbol
-; si - counter of ramka high    | destroy
-; 
+; ax - str num                  | save
+; bx - color & symbol           | save
+; si - counter of ramka high    | destroy to start bp value
+; bp - ramka width              | destroy to 0
+; arg_1, arg_2, arg_3, arg_4 - symbols for ramka
 ;--------------------------
 draw_ramka proc 
         mov si, 1
+        mov di, sp
 
-        draw_one_ramka:
-            mov bl, 19h
-            add cx, 2           ; cx' = cx + 2 -> wide of ramka
+            draw_one_ramka:
+        xor bl, bl
+        add bx, ss:[di + 2]
+        add cx, 2           ; cx' = cx + 2 -> wide of ramka
 
-                push si
-            push bx
-            push cx             ; str len - arg2 for func
-            sub ax, si
-            push ax             ; str num - arg 3 for func
-            call print_symbols_horizontal_string
-                pop si
+        push bx             ; -> but down - arg1
 
-                push si
-            mov bl, 18h
-            push bx             ; '->' but up - arg 1
-            push cx             ; str len - srg 2
-            add ax, si          ; str num - arg 3
-            add ax, si
-            push ax
-            call print_symbols_horizontal_string
-                pop si
+        push cx             ; str len - arg2 for func
 
-                push si
-            mov bl, 1ah
-            push bx             ; ->, arg 4
-            shl si, 1
-            sub ax, si           ; high string
-            add si, 1
-            push si             ; vertical str len, arg 3
-            mov dx, 80d
-            sub dx, cx          ; dx = column num
-            and dx, 0FFFEh
-            push dx             ; column num, arg 2
-            push ax             ; str num, arg 1
-            call print_symbols_vertical_string
-            add sp, 8           ; cleaning stack
-                pop si
+        sub ax, si
+        push ax             ; str num - arg 3 for func
 
-                push si
-            mov bl, 1bh
-            push bx          ; <-
-            shl si, 1
-            add si, 1
-            push si
-            add dx, cx
-            add dx, cx
-            dec dx              ; want to floor(dx)
-            and dx, 0FFFEh
-            push dx
-            push ax
-            call print_symbols_vertical_string
-            add sp, 8           ; cleaning stack
-                pop si
+        call print_symbols_horizontal_string
+        
+        
+        xor bl, bl
+        add bx, ss:[di + 4]
+        push bx             ; '->' but up - arg 1
 
-            add ax, si
-            add si, 1
-            add bh, 1
+        push cx             ; str len - srg 2
 
-            dec bp
-            cmp bp, 0
-            jg draw_one_ramka
+        add ax, si
+        add ax, si
+        push ax             ; str num - arg 3
+
+        call print_symbols_horizontal_string
+
+
+
+        push si             ; saving
+
+
+
+        xor bl, bl
+        add bx, ss:[di + 8]
+        push bx             ; ->, arg 4
+
+        shl si, 1
+        sub ax, si           ; high string
+        add si, 1
+        push si             ; vertical str len, arg 3
+
+        mov dx, 80d
+        sub dx, cx          ; dx = column num
+        and dx, 0FFFEh
+        push dx             ; column num, arg 2
+
+        push ax             ; str num, arg 1
+
+        call print_symbols_vertical_string
+        add sp, 8           ; cleaning stack
+        
+        
+
+        xor bl, bl
+        add bx, ss:[di + 6]
+        push bx          ; <- arg 4
+        
+        push si         ; vertical str len, arg 3
+        
+        mov dx, 80d
+        add dx, cx          ; dont changing cx
+        dec dx              ; want to floor(dx)
+        and dx, 0FFFEh
+        push dx             ; column num, arg 2
+
+        push ax             ; str num, arg 1
+
+        call print_symbols_vertical_string
+        add sp, 8           ; cleaning stack
+
+
+        pop si
+        add ax, si
+        add si, 1
+        add bh, 1
+
+        dec bp
+        cmp bp, 0
+        jg draw_one_ramka
+
 
         ret
 ;--------------------------
+
+Hello                   db "Note: In argument string must be 5 hex numbers, all for 2 symbols: ramka latter & back color | string num | ramka width | user string color | ramka style$"  
+
 end		 start
